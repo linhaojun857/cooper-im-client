174eded381b190bbc606ddd163808ce7bad4fd02
linhaojun857 <lhj13401792631@163.com>
2023-10-10 20:56:35 +0800

    cooper-im-client

--- view/LRWidget.cpp
+++ view/LRWidget.cpp
@@ -8,6 +8,12 @@ LRWidget::LRWidget(QWidget* parent) : QWidget(parent), ui(new Ui::LRWidget) {
     ui->setupUi(this);
     this->setWindowTitle("Cooper");
     ui->m_lRTabWidget->tabBar()->hide();
+    connect(ui->m_registerPushButton, &QPushButton::clicked, this, [&]() {
+        ui->m_lRTabWidget->setCurrentIndex(1);
+    });
+    connect(ui->m_loginPushButton_r, &QPushButton::clicked, this, [&]() {
+        ui->m_lRTabWidget->setCurrentIndex(0);
+    });
 }
 
 LRWidget::~LRWidget() {
